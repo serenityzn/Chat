0ea27688af5ea75b332af54060d2e2063c9037dd
maintainer <maintainer@localhost.localdomain>
2014-04-29 10:01:32 -0400

    /me/ added

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from twisted.internet import reactor
  
 class Server(Protocol):
   name = ""
-  
+  clname = ""
   def getName(self):
 	if self.name!="":
 		return self.name
@@ -14,6 +14,7 @@ class Server(Protocol):
 	self.factory.sendtoall(self.getName()+" has joined the chat.")
 	self.factory.clientsProtocols[0].append(self)
 	self.factory.clientsProtocols[1].append(self.getName())
+	self.clname = self.getName()
 
   def sendLine(self, line):
 	self.transport.write(line+"\r\n")
@@ -30,8 +31,13 @@ class Server(Protocol):
 	elif data[:4] == "/to/":
 		spl = data[4:].split(">")
 		self.factory.sendprivate(spl[0], spl[1])
+	elif data[:4] == "/me/":
+		spl = data[4:].split("\r\n")
+		self.clname = spl[0]
+		
 	else:
-		self.factory.sendtoall("["+self.getName()+"]"+data)
+		self.factory.sendtoall("["+self.clname+"]"+data)
+		
  
 class ServerFactory(Factory):
 	protocol = Server
@@ -42,7 +48,7 @@ class ServerFactory(Factory):
 		for client in self.clientsProtocols[0]:
 			client.sendLine(msg)
 	def sendprivate(self, cl, msg):
-		index = self.clientsProtocols[1].index(cl)
+		index = self.clientsProtocols[2].index(cl)
 		self.clientsProtocols[0][index].sendLine(msg)
 
 
